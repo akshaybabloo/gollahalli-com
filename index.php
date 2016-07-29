f61b493f37fa16dd9b7fe124050e850a0c5dbbac
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-07-30 10:16:52 +1200

    Revert "blog removed"
    This reverts commit e20a17927f94edc61f873e86b6ecb523cac258fc.

--- index.php
+++ index.php
@@ -193,9 +193,9 @@
                 <li>
                     <a class="page-scroll" href="#tutorial">Tutorials</a>
                 </li>
-                <!-- <li>
+                <li>
                     <a class="page-scroll" href="#blog">Blog</a>
-                </li> -->
+                </li>
                 <li>
                     <a class="page-scroll" href="#contact">Contact</a>
                 </li>
@@ -985,39 +985,39 @@
 </div>
 
 <!--========================= Blog ===========================-->
-<!-- <section class="bg-primary less-bottom" id="blog">
+<section class="bg-primary less-bottom" id="blog">
     <div class="container text-center">
         <h2>Blog</h2>
         <hr class="light">
     </div>
     <div class="container">
         <div class="row">
-            // <?php
-            // $url = "https://blog.gollahalli.me/rss/";
-            // $content = file_get_contents($url);
+            <?php
+            $url = "https://blog.gollahalli.me/rss/";
+            $content = file_get_contents($url);
 
-            // $x = new SimpleXmlElement($content);
+            $x = new SimpleXmlElement($content);
 
-            // foreach ($x->channel->item as $entry) {
-            //     echo "<div class='mas-container mas-col3'><div class='grid-sizer'></div><div class='blog-item3 item'>";
-            //     $nameSpace1 = $entry->getNameSpaces(True);
-            //     $media = $entry->children('media', True);
-            //     $dc = $entry->children($nameSpace1['dc']);
-            //     $newDateString = date_format(date_create_from_format('D, d M Y G:i:s T', $entry->pubDate), 'd M Y');
+            foreach ($x->channel->item as $entry) {
+                echo "<div class='mas-container mas-col3'><div class='grid-sizer'></div><div class='blog-item3 item'>";
+                $nameSpace1 = $entry->getNameSpaces(True);
+                $media = $entry->children('media', True);
+                $dc = $entry->children($nameSpace1['dc']);
+                $newDateString = date_format(date_create_from_format('D, d M Y G:i:s T', $entry->pubDate), 'd M Y');
 
-            //     if ($media) {
-            //         $url = $media->content->attributes();
-            //         echo "<img class='hover_style3' src='$url' alt='$entry->title'>";
-            //     }
+                if ($media) {
+                    $url = $media->content->attributes();
+                    echo "<img class='hover_style3' src='$url' alt='$entry->title'>";
+                }
 
-            //     echo "<div class='top-bar'><span><strong><i class='ion-person-stalker'></i></strong>" . " " . $dc->creator . "</span><span><i class='ion-clock'></i>" . " " . $newDateString . "</span></div>";
-            //     echo "<a href='$entry->link' title='$entry->link'><h4><b>" . $entry->title . "</b></h4></a>";
-            //     echo "<p>" . $entry->description . "</p>";
-            //     echo "<div class='blog_readmore_btn_holder'><a href='$entry->link' class='more-link'><div class='blog_readmore_btn3'>" . "Continue reading" . "<i class='ion-arrow-right-c'></i></div></a></div>";
-            //     echo "</div></div>";
-            // }
+                echo "<div class='top-bar'><span><strong><i class='ion-person-stalker'></i></strong>" . " " . $dc->creator . "</span><span><i class='ion-clock'></i>" . " " . $newDateString . "</span></div>";
+                echo "<a href='$entry->link' title='$entry->link'><h4><b>" . $entry->title . "</b></h4></a>";
+                echo "<p>" . $entry->description . "</p>";
+                echo "<div class='blog_readmore_btn_holder'><a href='$entry->link' class='more-link'><div class='blog_readmore_btn3'>" . "Continue reading" . "<i class='ion-arrow-right-c'></i></div></a></div>";
+                echo "</div></div>";
+            }
 
-            // ?>
+            ?>
         </div>
 
     </div>
@@ -1025,7 +1025,7 @@
     <p>
         <a href="https://blog.gollahalli.me" class="btn center-block bigger">Read more</a>
     </p>
-</section> -->
+</section>
 
 <!--========================= Contact Me ===========================-->
 <section id="contact">
