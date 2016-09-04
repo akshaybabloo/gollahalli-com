abd2c10b5f0965a9a888b63f1b3ed22d3ba1e3b8
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-09-04 22:50:18 +1200

    common stuff in header.php

--- pages/publication.php
+++ pages/publication.php
@@ -1,9 +1,6 @@
 <?php
-$json = file_get_contents('pages/content.json');
 
-$results1 = json_decode($json);
-
-foreach ($results1->publication as $key => $jsons) {
+foreach ($results->publication as $key => $jsons) {
     echo '<h3>' . ucwords($key) . '</h3>';
     echo '<hr>';
     echo '<ol class="text-justify">';
--- pages/skills.php
+++ pages/skills.php
@@ -1,9 +1,6 @@
 <?php
-$json_skills = file_get_contents('pages/content.json');
 
-$results_skills = json_decode($json_skills);
-
-foreach ($results_skills->skills as $key => $jsons) {
+foreach ($results->skills as $key => $jsons) {
     foreach ($jsons as $value) {
         echo '<tr>';
         echo '<td class="col-md-2">'.ucwords($key).'</td>';
