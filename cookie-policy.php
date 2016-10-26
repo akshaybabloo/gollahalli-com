4289c9fff6c850161d4fb8939042c9cfb518082c
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-10-27 12:15:02 +1300

    html -> php and we -> I

--- cookie-policy.php
+++ cookie-policy.php
@@ -1,3 +1,5 @@
+<!-- PHP header -->
+<?php require 'pages/header.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,66 +17,50 @@
     <title>Akshay Raj Gollahalli - Cookie Policy</title>
 
     <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $cdn ?>/css/bootstrap.min.css" type="text/css">
 
     <!-- Custom Fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Cousine' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
-          rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
-          rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,500i,700,900" rel="stylesheet" type='text/css'>
 
-    <!-- Plugin CSS -->
-    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/gollahalli.css" type="text/css">
-    <link rel="stylesheet" href="css/creative.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $cdn ?>/css/creative.css" type="text/css">
 
     <!-- For old IEs -->
-    <link rel="shortcut icon" href="img/favicon/favicon.ico"/>
+    <link rel="shortcut icon" href="<?php echo $cdn ?>/img/favicon/favicon.ico"/>
 
     <!-- For new browsers - multisize ico  -->
-    <link rel="icon" type="image/x-icon" sizes="16x16 32x32" href="img/favicon/favicon.ico">
+    <link rel="icon" type="image/x-icon" sizes="16x16 32x32" href="<?php echo $cdn ?>/img/favicon/favicon.ico">
 
     <!-- For iPad with high-resolution Retina display running iOS ≥ 7: -->
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/favicon/favicon-152.png">
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo $cdn ?>/img/favicon/favicon-152.png">
 
     <!-- For iPad with high-resolution Retina display running iOS ≤ 6: -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/favicon/favicon-144.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $cdn ?>/img/favicon/favicon-144.png">
 
     <!-- For iPhone with high-resolution Retina display running iOS ≥ 7: -->
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="img/favicon/favicon-120.png">
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo $cdn ?>/img/favicon/favicon-120.png">
 
     <!-- For iPhone with high-resolution Retina display running iOS ≤ 6: -->
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon/favicon-114.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $cdn ?>/img/favicon/favicon-114.png">
 
     <!-- For iPhone 6+ -->
-    <link rel="apple-touch-icon-precomposed" sizes="180x180" href="img/favicon/favicon-180.png">
+    <link rel="apple-touch-icon-precomposed" sizes="180x180" href="<?php echo $cdn ?>/img/favicon/favicon-180.png">
 
     <!-- For first- and second-generation iPad: -->
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon/favicon-72.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $cdn ?>/img/favicon/favicon-72.png">
 
     <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
-    <link rel="apple-touch-icon-precomposed" href="img/favicon/favicon-57.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo $cdn ?>/img/favicon/favicon-57.png">
 
     <!-- For Old Chrome -->
-    <link rel="icon" href="img/favicon/favicon-32.png" sizes="32x32">
+    <link rel="icon" href="<?php echo $cdn ?>/img/favicon/favicon-32.png" sizes="32x32">
 
     <!-- For IE10 Metro -->
     <meta name="msapplication-TileColor" content="#FFFFFF">
-    <meta name="msapplication-TileImage" content="favicon/img/favicon-144.png">
+    <meta name="msapplication-TileImage" content="<?php echo $cdn ?>/favicon/img/favicon-144.png">
 
     <!-- Chrome for Android -->
-    <link rel="icon" sizes="192x192" href="img/favicon/favicon-192.png">
+    <link rel="icon" sizes="192x192" href="<?php echo $cdn ?>/img/favicon/favicon-192.png">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
 
@@ -90,8 +76,8 @@
                     files
                     that are
                     downloaded to your computer, to improve your experience. This page describes what information they
-                    gather, how we use it
-                    and why we sometimes need to store these cookies. We will also share how you can prevent these
+                    gather, how I use it
+                    and why I sometimes need to store these cookies. I will also share how you can prevent these
                     cookies
                     from being stored
                     however this may downgrade or 'break' certain elements of the sites functionality.</p>
@@ -99,9 +85,9 @@
                 <p>For more general information on cookies see the Wikipedia article on <a
                         href="https://en.wikipedia.org/wiki/HTTP_cookie">HTTP Cookies</a>...</p>
 
-                <h2>How We Use Cookies?</h2>
+                <h2>How I Use Cookies?</h2>
 
-                <p>We use cookies for a variety of reasons detailed below. Unfortunately, in most cases there are no
+                <p>I use cookies for a variety of reasons detailed below. Unfortunately, in most cases there are no
                     industry standard
                     options for disabling cookies without completely disabling the functionality and features they add
                     to
@@ -123,27 +109,27 @@
                     Therefore
                     it is recommended that you do not disable cookies.</p>
 
-                <h2>The Cookies We Set</h2>
+                <h2>The Cookies I Set</h2>
 
 
-                <p>To provide you with a great experience on this site we provide the functionality to set your
+                <p>To provide you with a great experience on this site I provide the functionality to set your
                     preferences
                     for
-                    how this site runs when you use it. To remember your preferences we need to set cookies so that this
+                    how this site runs when you use it. To remember your preferences I need to set cookies so that this
                     information can be called whenever you interact with a page is affected by your preferences.</p>
 
                 <h2>Third Party Cookies</h2>
 
-                <p>In some special cases we also use cookies provided by trusted third parties. The following section
+                <p>In some special cases I also use cookies provided by trusted third parties. The following section
                     details which third
                     party cookies you might encounter through this site.</p>
 
                 <p>This site uses Google Analytics, which is one of the most widespread and trusted analytics solution
                     on
                     the web for
-                    helping us to understand how you use the site and ways that we can improve your experience. These
+                    helping us to understand how you use the site and ways that I can improve your experience. These
                     cookies may track
-                    things such as how long you spend on the site and the pages that you visit so we can continue to
+                    things such as how long you spend on the site and the pages that you visit so I can continue to
                     produce
                     engaging
                     content.</p>
@@ -151,14 +137,14 @@
                 <p>For more information on Google Analytics cookies, see the official <a
                         href="https://www.google.co.nz/analytics/">Google Analytics</a> page.</p>
 
-                <p>From time to time we test new features and make subtle changes to the way that the site is delivered.
-                    When we are still
+                <p>From time to time I test new features and make subtle changes to the way that the site is delivered.
+                    When I are still
                     testing new features these cookies may be used to ensure that you receive a consistent experience
                     while
                     on the site
-                    while ensuring we understand which optimisations our users appreciate the most.</p>
+                    while ensuring I understand which optimisations our users appreciate the most.</p>
 
-                <p>We also use social media buttons and/or plugins on this site that allow you to connect with your
+                <p>I also use social media buttons and/or plugins on this site that allow you to connect with your
                     social
                     network in
                     various ways. For these to work the following social media sites including; LinkedIn, Facebook and
