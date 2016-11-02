68f84f38ba2b1ac1295561294da60137b9059c6b
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-11-02 17:42:49 +1300

    error page updated

---
+++ 401.php
@@ -0,0 +1,21 @@
+<?php require 'pages/error/header.php' ?>
+<title>Forbidden</title>
+
+</head>
+<body data-gr-c-s-loaded="true">
+<div class="wrapper">
+    <div class="container">
+
+        <?php echo header_image(); ?>
+
+        <div class="content-primary">
+            <h1 class="title">Unauthorised</h1>
+            <p class="description">You are not authorised to view this page.</p>
+            <div class="section-footer">
+                <a href="https://www.gollahalli.me" class="button button-primary">Back To Homepage</a>
+                <a href="mailto:akshay@gollahalli.me?Subject=Unauthorised%20error" class="button button-default">Report Error</a>
+            </div>
+        </div>
+    </div>
+
+<?php require 'pages/error/footer.php' ?>
---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php require 'pages/error/header.php' ?>
+<title>Page Not Found</title>
+
+</head>
+<body data-gr-c-s-loaded="true">
+<div class="wrapper">
+    <div class="container">
+        <header class="header-primary">
+            <a href="https://www.gollahalli.me" rel="home"><img class="logo" src="//www.gollahalli.me/img/logo-sm.png" alt="logo"></a>
+        </header>
+
+        <div class="content-primary">
+            <h1 class="title">Page Not Found</h1>
+            <p class="description">The page you are looking for was moved, removed, <br>
+                renamed or might never existed.</p>
+            <div class="section-footer">
+                <a href="https://www.gollahalli.me" class="button button-primary">Back To Homepage</a>
+                <a href="mailto:akshay@gollahalli.me?Subject=404%20error" class="button button-default">Report Error</a>
+            </div>
+        </div>
+    </div>
+<?php require 'pages/error/footer.php' ?>
