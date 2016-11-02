ef4f1517b754eab3d01056e498bb85661f0c836e
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-11-02 15:06:41 +1300

    links added and updated

--- pages/amp/tutorials.php
+++ pages/amp/tutorials.php
@@ -6,7 +6,7 @@ function tutorials()
     $ret = '';
 
     foreach ($results->tutorials as $jsons) {
-        $ret .= "<a href='$jsons->title' class='blog-carousel-item'>";
+        $ret .= "<a href='$jsons->link' class='blog-carousel-item'>";
         $ret .= "<amp-img  src='. $cdn . '/img/portfolio/' . $jsons->title . '.jpg' width=125 height=150></amp-img>";
         $ret .= "<div class='blog-carousel-item-caption'>";
         $ret .= "<h5 class='margin-0'>" . $jsons->title . "</h5>";
