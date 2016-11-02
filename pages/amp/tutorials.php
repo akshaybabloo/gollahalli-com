4bb00887e3bd54a13e5756106e85b0a6db9d0afa
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-11-02 16:43:57 +1300

    scr link problem rectified

--- pages/amp/tutorials.php
+++ pages/amp/tutorials.php
@@ -7,7 +7,7 @@ function tutorials()
 
     foreach ($results->tutorials as $jsons) {
         $ret .= "<a href='$jsons->link' class='blog-carousel-item'>";
-        $ret .= "<amp-img  src='. $cdn . '/img/portfolio/' . $jsons->title . '.jpg' width=125 height=150></amp-img>";
+        $ret .= "<amp-img  src=". $cdn . "/img/portfolio/tut/" . $jsons->title . ".jpg width=125 height=150></amp-img>";
         $ret .= "<div class='blog-carousel-item-caption'>";
         $ret .= "<h5 class='margin-0'>" . $jsons->title . "</h5>";
         $ret .= "<small>Read more...</small>";
