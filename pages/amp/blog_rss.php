92eef3b41cc797c410b6418578dac10363d38dbb
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-11-02 14:54:19 +1300

    code reformatted

--- pages/amp/blog_rss.php
+++ pages/amp/blog_rss.php
@@ -10,16 +10,18 @@ function blog()
         $media = $entry->children('media', True);
 
         $ret .= "<a href='$entry->link' class='blog-carousel-item'>";
+
         if ($media) {
             $url = $media->content->attributes();
             $ret .= "<amp-img  src='$url' width=125 height=150></amp-img>";
         }else{
             $ret .= "<amp-img  src='#' width=125 height=150></amp-img>";
         }
-        $ret .= '<div class="blog-carousel-item-caption">
-                        <h5 class="margin-0">'.$entry->title.'</h5>
-                        <small>Read more...</small>
-                    </div></a>';
+
+        $ret .= "<div class='blog-carousel-item-caption'>";
+        $ret .= "<h5 class='margin-0'>".$entry->title."</h5>";
+        $ret .= "<small>Read more...</small>";
+        $ret .= "</div></a>";
     }
     return $ret;
 }
