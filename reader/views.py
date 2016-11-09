d6df7fab5f0c7a7a00caefa9d01db5a5d6b60894
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-11-09 13:13:14 +1300

    template and its function added

--- reader/views.py
+++ reader/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {}
+    template = "index.html"
+    return render(request, template, context)
