12f9b4f9582f08ad846f7029f467fc1d13b57d7d
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-11-14 23:51:17 +1300

    exception added

--- editor/views.py
+++ editor/views.py
@@ -9,6 +9,8 @@ def index(request):
         a = ContentModel.objects.get(ref_id='1')
     except ContentModel.DoesNotExist as e:
         return render(request, template, context)
+    except ContentModel.ProgrammingError as e:
+        return render(request, template, context)
     return render(request, template, context)
 
 
