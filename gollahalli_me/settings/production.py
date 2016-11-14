4de373c37eadec3a50377e9165a563f3dd50fdd8
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-11-15 00:48:38 +1300

    settings updated

--- gollahalli_me/settings/production.py
+++ gollahalli_me/settings/production.py
@@ -16,7 +16,8 @@ DATABASES = settings.DATABASES
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
