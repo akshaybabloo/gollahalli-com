e6dc4bde6714a278f557019388209a28591c8f29
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-11-16 00:58:17 +1300

    random avatar selection added

--- welcome/views.py
+++ welcome/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+import os
+from django.conf import settings
+from django.contrib.staticfiles.templatetags.staticfiles import static
+import random
 
 
-# Create your views here.
 def index(request):
-    context = {}
+    avatars = [av for av in os.listdir(settings.STATIC_ROOT + os.sep + 'img' + os.sep + 'avatar')]
+    temp = 'img' + os.sep + 'avatar' + os.sep + random.choice(avatars)
+    avatar_img = static(temp)
+    context = {'avatar_img': avatar_img}
+    # context = {}
     template = "welcome.html"
     return render(request, template, context)
