b96cd09bcf6b609cc7f5ff0b701f667edf4ac7da
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-11-15 17:51:08 +1300

    aws moved to production

--- gollahalli_me/settings/base.py
+++ gollahalli_me/settings/base.py
@@ -108,19 +108,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# S3
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_ACCESS_KEY_ID = 'AKIAIPQQUJGYXMFFOASA'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'gollahalli'
-AWS_HEADERS = {
-    'Expires': 'Thu, 15 Apr 2010 20:00:00 GMT',
-    'Cache-Control': 'max-age=86400',
-}
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_CUSTOM_DOMAIN = 'gollahalli-me-test-ypyxfkub.stackpathdns.com'
-
 # heroku config:set AWS_ACCESS_KEY_ID=AKIAIPQQUJGYXMFFOASA AWS_SECRET_ACCESS_KEY=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
 
 
--- gollahalli_me/settings/production.py
+++ gollahalli_me/settings/production.py
@@ -27,6 +27,20 @@ ALLOWED_HOSTS = ['*']
 
 SHARE_URL = "http://www.gollahalli.me/"
 
+# S3
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_ACCESS_KEY_ID = 'AKIAIPQQUJGYXMFFOASA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'gollahalli'
+AWS_HEADERS = {
+    'Expires': 'Thu, 15 Apr 2010 20:00:00 GMT',
+    'Cache-Control': 'max-age=86400',
+}
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_CUSTOM_DOMAIN = 'gollahalli-me-test-ypyxfkub.stackpathdns.com'
+
+
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
