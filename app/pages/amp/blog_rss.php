2f626d73a17521d99f2fd2d4317a6e80d716b4b8
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-11-19 03:44:18 +1300

    exception added

--- app/pages/amp/blog_rss.php
+++ app/pages/amp/blog_rss.php
@@ -3,25 +3,29 @@ function blog()
 {
     $url = "https://blog.gollahalli.me/rss/";
     $content = file_get_contents($url);
-
-    $x = new SimpleXmlElement($content);
     $ret = '';
-    foreach ($x->channel->item as $entry) {
-        $media = $entry->children('media', True);
 
-        $ret .= "<a href='$entry->link' class='blog-carousel-item'>";
+    try {
+        $x = new SimpleXmlElement($content);
+        foreach ($x->channel->item as $entry) {
+            $media = $entry->children('media', True);
 
-        if ($media) {
-            $url = $media->content->attributes();
-            $ret .= "<amp-img  src=" . addScheme($url). " width=125 height=150></amp-img>";
-        }else{
-            $ret .= "<amp-img  src='#' width=125 height=150></amp-img>";
+            $ret .= "<a href='$entry->link' class='blog-carousel-item'>";
+
+            if ($media) {
+                $url = $media->content->attributes();
+                $ret .= "<amp-img  src=" . addScheme($url) . " width=125 height=150></amp-img>";
+            } else {
+                $ret .= "<amp-img  src='#' width=125 height=150></amp-img>";
+            }
+
+            $ret .= "<div class='blog-carousel-item-caption'>";
+            $ret .= "<h5 class='margin-0'>" . $entry->title . "</h5>";
+            $ret .= "<small>Read more...</small>";
+            $ret .= "</div></a>";
         }
-
-        $ret .= "<div class='blog-carousel-item-caption'>";
-        $ret .= "<h5 class='margin-0'>".$entry->title."</h5>";
-        $ret .= "<small>Read more...</small>";
-        $ret .= "</div></a>";
+    } catch (Exception $e){
+        $ret = 'Under Maintenance';
     }
     return $ret;
 }
