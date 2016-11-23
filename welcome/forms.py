e15ca14d8318852d7ceab86ac2efa7be31c7d4ba
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-11-23 20:10:36 +1300

    forms updated

--- welcome/forms.py
+++ welcome/forms.py
@@ -4,16 +4,32 @@
 # Copyright 2016 Akshay Raj Gollahalli
 
 from django import forms
+from .models import WelcomeToAbies
 
 
 class WelcomeForm(forms.Form):
-    firs_name = forms.CharField(max_length=120, name='welcome_first_name')
-    last_name = forms.CharField(max_length=120, name='welcome_last_name')
-    email_id = forms.EmailField(name='welcome_email_id')
-    company_name = forms.CharField(max_length=240, name='welcome_company_name')
-    title_same_as_company = forms.CheckboxSelectMultiple()
-    # AWS_SECRET = forms.CharField(max_length=120, name='welcome_AWS_SECRET')
+    # page 1
+    first_name = forms.CharField(max_length=120, required=False)
+    last_name = forms.CharField(max_length=120, required=False)
+    profile_image_location = forms.FileField(required=False)
+    email = forms.EmailField(required=False)
+
+    # page 2
+    username = forms.CharField(max_length=120, required=False)
+    password = forms.CharField(widget=forms.PasswordInput, required=False)
+    confirm_password = forms.CharField(widget=forms.PasswordInput, required=False)
+    # AWS_SECRET = forms.CharField(max_length=120)
     # AWS_SECRET_KEY = forms.CharField(widget=forms.PasswordInput)
-    # AWS_S3_BUCKET_NAME = forms.CharField(max_length=120, name='welcome_AWS_S3_BUCKET_NAME')
-    root_username = forms.CharField(max_length=120, name='welcome_root_username')
-    root_password = forms.CharField(widget=forms.PasswordInput)
+    # AWS_S3_BUCKET_NAME = forms.CharField(max_length=120)
+
+    # page 3
+    company_name = forms.CharField(max_length=240, required=False)
+    company_logo_location = forms.FileField(required=False)
+    city = forms.CharField(max_length=120, required=False)
+    country = forms.CharField(max_length=120, required=False)
+
+
+# class WelcomeFormModel(forms.ModelForm):
+#     class Meta:
+#         model = WelcomeToAbies
+#         fields = ('welcome_first_name', 'welcome_last_name')
