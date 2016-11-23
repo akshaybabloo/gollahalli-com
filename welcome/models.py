0e359e19f97b623387af4e5f09c4654dfda5cb9d
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-11-24 02:55:22 +1300

    default, null changed and blank added

--- welcome/models.py
+++ welcome/models.py
@@ -5,20 +5,20 @@ class AbiesModel(models.Model):
     ref_id = models.CharField(primary_key=True, max_length=120, default='1', unique=True)
 
     # page 1
-    first_name = models.CharField(max_length=120, default='null', name='first_name')
-    last_name = models.CharField(max_length=120, default='null', name='last_name')
-    email = models.EmailField(name='email')
-    profile_image_location = models.ImageField(upload_to='images', name='profile_image_location')
+    first_name = models.CharField(max_length=120, name='first_name', blank=True)
+    last_name = models.CharField(max_length=120, name='last_name', blank=True)
+    email = models.EmailField(name='email', blank=True)
+    profile_image_location = models.ImageField(upload_to='images', name='profile_image_location', blank=True)
 
     # page 2
     # AWS_SECRET = models.CharField(max_length=120, default='null', name='welcome_AWS_SECRET')
     # AWS_SECRET_KEY = models.CharField(max_length=120, default='null', name='welcome_AWS_SECRET_KEY')
     # AWS_S3_BUCKET_NAME = models.CharField(max_length=120, default='null', name='welcome_AWS_S3_BUCKET_NAME')
-    username = models.CharField(max_length=120, default='null', name='username')
-    password = models.CharField(max_length=30, default='null', name='password')
+    username = models.CharField(max_length=120, name='username', blank=True)
+    password = models.CharField(max_length=30, name='password', blank=True)
 
     # page 3
-    company_logo_location = models.ImageField(upload_to='images', name='company_logo_location')
-    company_name = models.CharField(max_length=240, default='null', name='company_name')
-    city = models.CharField(max_length=240, default='null', name='city')
-    country = models.CharField(max_length=240, default='null', name='country')
+    company_logo_location = models.ImageField(upload_to='images', name='company_logo_location', blank=True)
+    company_name = models.CharField(max_length=240, name='company_name', blank=True)
+    city = models.CharField(max_length=240, name='city', blank=True)
+    country = models.CharField(max_length=240, name='country', blank=True)
