4baac293ac4be840deee1687d25ec717ad78cd9e
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-11-24 02:58:15 +1300

    See description
    * COUNTRY_LIST moved to forms.py
    * WelcomeFormModel is being used
    * View receives all form submissions now

--- welcome/views.py
+++ welcome/views.py
@@ -1,4 +1,3 @@
-import operator
 import os
 import random
 
@@ -6,80 +5,40 @@ from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.shortcuts import render
 
-from .forms import WelcomeForm
+from .forms import WelcomeFormModel
 from .models import AbiesModel
-
-COUNTRY_LIST = {"AF": "Afghanistan", "AX": "\u00c5land Islands", "AL": "Albania", "DZ": "Algeria",
-                "AS": "American Samoa", "AD": "Andorra", "AO": "Angola", "AI": "Anguilla", "AQ": "Antarctica",
-                "AG": "Antigua & Barbuda", "AR": "Argentina", "AM": "Armenia", "AW": "Aruba", "AC": "Ascension Island",
-                "AU": "Australia", "AT": "Austria", "AZ": "Azerbaijan", "BS": "Bahamas", "BH": "Bahrain",
-                "BD": "Bangladesh", "BB": "Barbados", "BY": "Belarus", "BE": "Belgium", "BZ": "Belize", "BJ": "Benin",
-                "BM": "Bermuda", "BT": "Bhutan", "BO": "Bolivia", "BA": "Bosnia & Herzegovina", "BW": "Botswana",
-                "BR": "Brazil", "IO": "British Indian Ocean Territory", "VG": "British Virgin Islands", "BN": "Brunei",
-                "BG": "Bulgaria", "BF": "Burkina Faso", "BI": "Burundi", "KH": "Cambodia", "CM": "Cameroon",
-                "CA": "Canada", "IC": "Canary Islands", "CV": "Cape Verde", "BQ": "Caribbean Netherlands",
-                "KY": "Cayman Islands", "CF": "Central African Republic", "EA": "Ceuta & Melilla", "TD": "Chad",
-                "CL": "Chile", "CN": "China", "CX": "Christmas Island", "CC": "Cocos (Keeling) Islands",
-                "CO": "Colombia", "KM": "Comoros", "CG": "Congo - Brazzaville", "CD": "Congo - Kinshasa",
-                "CK": "Cook Islands", "CR": "Costa Rica", "CI": "C\u00f4te d\u2019Ivoire", "HR": "Croatia",
-                "CU": "Cuba", "CW": "Cura\u00e7ao", "CY": "Cyprus", "CZ": "Czech Republic", "DK": "Denmark",
-                "DG": "Diego Garcia", "DJ": "Djibouti", "DM": "Dominica", "DO": "Dominican Republic", "EC": "Ecuador",
-                "EG": "Egypt", "SV": "El Salvador", "GQ": "Equatorial Guinea", "ER": "Eritrea", "EE": "Estonia",
-                "ET": "Ethiopia", "FK": "Falkland Islands", "FO": "Faroe Islands", "FJ": "Fiji", "FI": "Finland",
-                "FR": "France", "GF": "French Guiana", "PF": "French Polynesia", "TF": "French Southern Territories",
-                "GA": "Gabon", "GM": "Gambia", "GE": "Georgia", "DE": "Germany", "GH": "Ghana", "GI": "Gibraltar",
-                "GR": "Greece", "GL": "Greenland", "GD": "Grenada", "GP": "Guadeloupe", "GU": "Guam", "GT": "Guatemala",
-                "GG": "Guernsey", "GN": "Guinea", "GW": "Guinea-Bissau", "GY": "Guyana", "HT": "Haiti",
-                "HN": "Honduras", "HK": "Hong Kong SAR China", "HU": "Hungary", "IS": "Iceland", "IN": "India",
-                "ID": "Indonesia", "IR": "Iran", "IQ": "Iraq", "IE": "Ireland", "IM": "Isle of Man", "IL": "Israel",
-                "IT": "Italy", "JM": "Jamaica", "JP": "Japan", "JE": "Jersey", "JO": "Jordan", "KZ": "Kazakhstan",
-                "KE": "Kenya", "KI": "Kiribati", "XK": "Kosovo", "KW": "Kuwait", "KG": "Kyrgyzstan", "LA": "Laos",
-                "LV": "Latvia", "LB": "Lebanon", "LS": "Lesotho", "LR": "Liberia", "LY": "Libya", "LI": "Liechtenstein",
-                "LT": "Lithuania", "LU": "Luxembourg", "MO": "Macau SAR China", "MK": "Macedonia", "MG": "Madagascar",
-                "MW": "Malawi", "MY": "Malaysia", "MV": "Maldives", "ML": "Mali", "MT": "Malta",
-                "MH": "Marshall Islands", "MQ": "Martinique", "MR": "Mauritania", "MU": "Mauritius", "YT": "Mayotte",
-                "MX": "Mexico", "FM": "Micronesia", "MD": "Moldova", "MC": "Monaco", "MN": "Mongolia",
-                "ME": "Montenegro", "MS": "Montserrat", "MA": "Morocco", "MZ": "Mozambique", "MM": "Myanmar (Burma)",
-                "NA": "Namibia", "NR": "Nauru", "NP": "Nepal", "NL": "Netherlands", "NC": "New Caledonia",
-                "NZ": "New Zealand", "NI": "Nicaragua", "NE": "Niger", "NG": "Nigeria", "NU": "Niue",
-                "NF": "Norfolk Island", "KP": "North Korea", "MP": "Northern Mariana Islands", "NO": "Norway",
-                "OM": "Oman", "PK": "Pakistan", "PW": "Palau", "PS": "Palestinian Territories", "PA": "Panama",
-                "PG": "Papua New Guinea", "PY": "Paraguay", "PE": "Peru", "PH": "Philippines", "PN": "Pitcairn Islands",
-                "PL": "Poland", "PT": "Portugal", "PR": "Puerto Rico", "QA": "Qatar", "RE": "R\u00e9union",
-                "RO": "Romania", "RU": "Russia", "RW": "Rwanda", "WS": "Samoa", "SM": "San Marino",
-                "ST": "S\u00e3o Tom\u00e9 & Pr\u00edncipe", "SA": "Saudi Arabia", "SN": "Senegal", "RS": "Serbia",
-                "SC": "Seychelles", "SL": "Sierra Leone", "SG": "Singapore", "SX": "Sint Maarten", "SK": "Slovakia",
-                "SI": "Slovenia", "SB": "Solomon Islands", "SO": "Somalia", "ZA": "South Africa",
-                "GS": "South Georgia & South Sandwich Islands", "KR": "South Korea", "SS": "South Sudan", "ES": "Spain",
-                "LK": "Sri Lanka", "BL": "St. Barth\u00e9lemy", "SH": "St. Helena", "KN": "St. Kitts & Nevis",
-                "LC": "St. Lucia", "MF": "St. Martin", "PM": "St. Pierre & Miquelon", "VC": "St. Vincent & Grenadines",
-                "SD": "Sudan", "SR": "Suriname", "SJ": "Svalbard & Jan Mayen", "SZ": "Swaziland", "SE": "Sweden",
-                "CH": "Switzerland", "SY": "Syria", "TW": "Taiwan", "TJ": "Tajikistan", "TZ": "Tanzania",
-                "TH": "Thailand", "TL": "Timor-Leste", "TG": "Togo", "TK": "Tokelau", "TO": "Tonga",
-                "TT": "Trinidad & Tobago", "TA": "Tristan da Cunha", "TN": "Tunisia", "TR": "Turkey",
-                "TM": "Turkmenistan", "TC": "Turks & Caicos Islands", "TV": "Tuvalu", "UM": "U.S. Outlying Islands",
-                "VI": "U.S. Virgin Islands", "UG": "Uganda", "UA": "Ukraine", "AE": "United Arab Emirates",
-                "GB": "United Kingdom", "US": "United States", "UY": "Uruguay", "UZ": "Uzbekistan", "VU": "Vanuatu",
-                "VA": "Vatican City", "VE": "Venezuela", "VN": "Vietnam", "WF": "Wallis & Futuna",
-                "EH": "Western Sahara", "YE": "Yemen", "ZM": "Zambia", "ZW": "Zimbabwe"}
 
 
 def index(request):
     if request.method == 'POST':
-        form = WelcomeForm(request.POST, request.FILES)
+        form = WelcomeFormModel(request.POST, request.FILES)
         if form.is_valid():
-            # firs_name = form.cleaned_data['first_name']
-            newdoc = AbiesModel(welcome_profile_image_location=request.FILES['profile_image_location'])
-            newdoc.save()
+            firs_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            company_name = form.cleaned_data['company_name']
+            city = form.cleaned_data['city']
+            country = form.cleaned_data['country']
+
+            profile_image_location = request.FILES.get('profile_image_location', None)
+            company_logo_location = request.FILES.get('company_logo_location', None)
+
+            if profile_image_location is not None:
+                newdoc = AbiesModel(welcome_profile_image_location=request.FILES['profile_image_location'])
+                newdoc.save()
+            if company_logo_location is not None:
+                newdoc = AbiesModel(welcome_profile_image_location=request.FILES['company_logo_location'])
+                newdoc.save()
+            print(firs_name, last_name, email, username, company_name, city, country)
         else:
             print('not valid')
     else:
-        form = WelcomeForm()
+        form = WelcomeFormModel()
 
     avatars = [av for av in os.listdir(settings.STATIC_ROOT + os.sep + 'img' + os.sep + 'avatar')]
     random_image_location = 'img' + os.sep + 'avatar' + os.sep + random.choice(avatars)
     avatar_img = static(random_image_location)
     template = "welcome.html"
-    context = {'avatar_img': avatar_img, 'county_list': sorted(COUNTRY_LIST.items(), key=operator.itemgetter(1)),
-               'form': form}
+    context = {'avatar_img': avatar_img, 'form': form}
     return render(request, template, context)
