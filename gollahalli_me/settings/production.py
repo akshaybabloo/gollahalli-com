4be29cadadf1aa90e80e797c34f1ae32a3fcee13
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-12-12 14:36:13 +0530

    custom domain updated

--- gollahalli_me/settings/production.py
+++ gollahalli_me/settings/production.py
@@ -38,7 +38,8 @@ AWS_HEADERS = {
     'Cache-Control': 'max-age=86400',
 }
 AWS_QUERYSTRING_AUTH = False
-AWS_S3_CUSTOM_DOMAIN = 'gollahalli-heroku-django-ypyxfkub.stackpathdns.com'
+# AWS_S3_CUSTOM_DOMAIN = 'gollahalli-heroku-django-ypyxfkub.stackpathdns.com'
+AWS_S3_CUSTOM_DOMAIN = 'https://s3.amazonaws.com/gollahalli-me-django'
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storage.StaticStorage'
