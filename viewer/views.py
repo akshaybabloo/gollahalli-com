7740405c92e77f2786e6e63a6eb33ae18c703910
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-12-23 15:39:19 +0530

    get_education added and sorted

--- viewer/views.py
+++ viewer/views.py
@@ -63,7 +63,10 @@ class ContentDecode:
         return self.json['about_me']['github']
 
     def get_education(self):
-        return self.json['about_me']['education']
+
+        json_data = self.json['about_me']['education']
+        a = [json_data[a] for a in sorted(json_data.keys(), reverse=True)]
+        return a
 
     def get_my_image(self):
 
