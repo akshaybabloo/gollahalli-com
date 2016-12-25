b6fdecfafaf3e826938706b2a54feae748bde9c5
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-12-25 17:23:49 +0530

    url names changed

--- editor/urls.py
+++ editor/urls.py
@@ -8,6 +8,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^login/', views.login, name='index'),
+    url(r'^$', views.index, name='index_main'),
+    url(r'^login/', views.login, name='index_login'),
 ]
