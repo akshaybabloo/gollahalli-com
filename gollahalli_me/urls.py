69e3df25084ce1ad5d6802005b7098adb140cc21
akshay raj gollahalli <akshaybabloo@gmail.com>
2016-12-28 16:37:02 +0530

    logout link added

--- gollahalli_me/urls.py
+++ gollahalli_me/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 from .sitemaps import *
 
@@ -15,7 +15,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls, name='admin_urls'),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
         name='django.contrib.sitemaps.views.sitemap'),
-    url(r'^accounts/login/$', login, {'template_name': 'admin/login.html'}, name="my_login")
+    url(r'^accounts/login/$', login, {'template_name': 'admin/login.html'}, name="my_login"),
+    url(r'^accounts/logout/$', logout),
 ]
 
 handler404 = 'viewer.views.page_not_found'
