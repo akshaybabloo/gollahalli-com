5fa7c00d9419d59708ee4e81d0bb93607b943188
akshay raj gollahalli <akshaybabloo@gmail.com>
2017-01-02 00:14:15 +0530

    sitemap index added

--- gollahalli_me/urls.py
+++ gollahalli_me/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import views
 from django.contrib.auth.views import login, logout
 from django.http import HttpResponse
-
 
 from .sitemaps import *
 
@@ -15,12 +14,15 @@ urlpatterns = [
     url(r'^', include('viewer.urls')),
     url(r'^editor/', include('editor.urls'), name='editor_urls'),
     url(r'^admin/', admin.site.urls, name='admin_urls'),
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
-        name='django.contrib.sitemaps.views.sitemap'),
     url(r'^accounts/login/$', login, {'template_name': 'admin/login.html'}, name="my_login"),
     url(r'^accounts/logout/$', logout),
-    url(r'^robots.txt', lambda x: HttpResponse("Sitemap: https://www.gollahalli.me/sitemap.xml\nUser-agent: *\nDisallow: /admin/", content_type="text/plain"),
-        name="robots_file"),
+    url(r'^robots.txt',
+        lambda x: HttpResponse("Sitemap: https://www.gollahalli.me/sitemap.xml\nUser-agent: *\nDisallow: /admin/",
+                               content_type="text/plain"), name="robots_file"),
+    url(r'^sitemap\.xml$', views.index, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^sitemap-(?P<section>.+).xml$', views.sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 handler404 = 'viewer.views.page_not_found'
