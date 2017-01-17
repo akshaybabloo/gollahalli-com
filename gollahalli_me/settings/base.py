208497b9dcbe8374d1cdcae107c97d574ccfc13c
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-01-17 14:58:02 +1300

    copied from common to base

--- gollahalli_me/settings/base.py
+++ gollahalli_me/settings/base.py
@@ -113,5 +113,36 @@ STATIC_URL = '/static/'
 # heroku config:set AWS_ACCESS_KEY_ID=AKIAIPQQUJGYXMFFOASA AWS_SECRET_ACCESS_KEY=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
 
 
-# Common stuff
-from .common import *
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'static_dirs'),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+            ],
+        },
+    },
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
