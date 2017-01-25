018997d66e8cb444db1a407e6c75114c3db00c9f
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-01-25 20:16:05 +1300

    gulp task for minify css and js added

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+var cleanCss = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+gulp.task('pack-js', function () {
+    return gulp.src([
+        'gollahalli_me/static/static_dirs/js/fitvids.js',
+        'gollahalli_me/static/static_dirs/js/script.js'
+    ])
+        .pipe(minify({
+            ext: {
+                min: '.js'
+            },
+            noSource: true
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('gollahalli_me/static/static_dirs/js'));
+});
+
+gulp.task('pack-css', function () {
+    return gulp.src([
+        'gollahalli_me/static/static_dirs/css/gollahalli.css',
+        'gollahalli_me/static/static_dirs/css/magnific-popup.css',
+        'gollahalli_me/static/static_dirs/css/style.css',
+        'gollahalli_me/static/static_dirs/css/owl.carousel.css',
+        'gollahalli_me/static/static_dirs/css/linea.css'
+    ])
+        .pipe(cleanCss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('gollahalli_me/static/static_dirs/css'));
+});
+
+gulp.task('default', ['pack-js', 'pack-css']);
