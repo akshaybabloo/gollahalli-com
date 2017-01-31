d526042f152346639ee390d95b873c1450820012
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-01-31 21:46:19 +1300

    update or create a model added for editor

--- editor/models.py
+++ editor/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class ContentModel(models.Model):
-    ref_id = models.CharField(primary_key=True, max_length=120, default='1', unique=True)
-    content = models.TextField(default="Write about you.")
+    ref_id = models.CharField(primary_key=True, max_length=120, unique=True)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        unique_together = ("content", "ref_id")
--- editor/views.py
+++ editor/views.py
@@ -2,21 +2,39 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseServerError
 from django.shortcuts import redirect
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
-from editor.models import ContentModel
+from .models import ContentModel
+from .forms import ContentFormModel
+import utility
+
+# ============================================================================================
+#                                       Form Main
+# ============================================================================================
 
 
 @login_required
 def index(request):
-
     template = "editor/home.html"
+
     try:
         json_content = ContentModel.objects.get(ref_id='1')
     except Exception:
         raise HttpResponseServerError
 
-    context = {'content': json_content.content}
+    if request.method == 'POST':
+        form = ContentFormModel(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data.get('content')
+            new_content, created = ContentModel.objects.update_or_create(ref_id='1')
+            new_content.content = form_data
+            new_content.save()
+
+            return HttpResponseRedirect('/editor')
+    else:
+        form = ContentFormModel()
+
+    context = {'content': json_content.content, 'form': form}
     return render(request, template, context)
 
 
