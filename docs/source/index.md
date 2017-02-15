93327ab22235d7e0a0db1db718fe89008c54fd8d
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-02-15 19:36:51 +1300

    docs added

---
+++ docs/source/index.md
@@ -0,0 +1,22 @@
+.. Akshay Raj Gollahalli documentation master file, created by
+   sphinx-quickstart on Wed Feb 15 19:25:12 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Akshay Raj Gollahalli's documentation!
+=================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   utility
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- utility.py
+++ utility.py
@@ -2,6 +2,28 @@ import json
 
 
 def is_json(my_json):
+    """
+    Checks if the input data is of JSON type.
+
+    Parameters
+    ----------
+    my_json: JSON Object
+        JSON Object to check.
+
+    Returns
+    -------
+    bool
+        `True` or `False`
+
+
+    Examples
+    --------
+
+    >>> json_string = {"hello": "hi"}
+    >>> is_json(json_string)
+    True
+
+    """
     try:
         json_object = json.loads(my_json)
         del json_object
@@ -11,4 +33,25 @@ def is_json(my_json):
 
 
 def format_date_time(date_time):
+    """
+    Formats Django date and time to Python `datetime`
+
+    Parameters
+    ----------
+    date_time: datetime
+        `datetime` from the database.
+
+    Returns
+    -------
+    datetime
+        formatted datetime
+
+    Examples
+    --------
+
+    >>> import datetime
+    >>> format_date_time(datetime.datetime.now())
+    Feb. 15, 2017, 07:12 PM
+
+    """
     return date_time.strftime('%b. %d, %Y, %I:%M %p')
