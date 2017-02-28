0b340a2eab58da8cee09927c4bf894cd0b9c2745
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-03-01 12:27:40 +1300

    AboutMe and PortfolioModel created

--- editor/models.py
+++ editor/models.py
@@ -4,7 +4,27 @@ from django.contrib.postgres.fields import JSONField
 
 class ContentModel(models.Model):
     ref_id = models.IntegerField(primary_key=True)
-    content = JSONField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    website_name = models.CharField(max_length=300, default="Enter your companies name")
+
+
+class AboutMeModel(models.Model):
+    ref_id = models.ForeignKey(ContentModel)
+    cv = models.CharField(default='CV URL', max_length=400)
+    bio = models.CharField(default='Your Bio', max_length=1000)
+    url = models.URLField(default='Website URL', max_length=400)
+    name = models.CharField(default='Your Name', max_length=400)
+    contact = models.EmailField(default='Your email ID', max_length=400)
+    github = models.URLField(default='GitHub URL', max_length=400)
+    twitter = models.URLField(default='Twitter URL', max_length=400)
+    linkedin = models.URLField(default='LinkedIn URL', max_length=400)
+    education_json = JSONField()
+    experience_json = JSONField()
+    publication_json = JSONField()
+    research_area_json = JSONField()
+    skills_t1_json = JSONField()
+    skills_t2_json = JSONField()
+
+
+class PortfolioModel(models.Model):
+    ref_id = models.ForeignKey(ContentModel)
+    projects_json = JSONField()
+    tutorials_json = JSONField()
