575049fa50bc55c6fd8296d23a567efdf0ceff45
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-03-01 12:29:23 +1300

    created, updated and website_name added

--- editor/models.py
+++ editor/models.py
@@ -4,6 +4,9 @@ from django.contrib.postgres.fields import JSONField
 
 class ContentModel(models.Model):
     ref_id = models.IntegerField(primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    website_name = models.CharField(max_length=300, default="Enter your companies name")
 
 
 class AboutMeModel(models.Model):
