2d884b5e7e4576f54b0e2e3105c0d1226ca30465
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-03-01 17:45:47 +1300

    Content, About and Portfolio forms added

--- editor/forms.py
+++ editor/forms.py
@@ -1,33 +1,22 @@
 from django import forms
 
-import utility
-from .models import ContentModel
-
-
-class ContentForm(forms.Form):
-    content = forms.CharField(widget=forms.Textarea, required=True)
-
-    def clean(self):
-        data = super(ContentForm, self).clean()
-        content = data.get('content')
-        if not utility.is_json(content):
-            raise forms.ValidationError('The entered content is not a valid JSON string.', code='invalid')
-
-        return content
-
-    def equal(self, value):
-        raise forms.ValidationError(value)
+from .models import ContentModel, AboutMeModel, PortfolioModel
 
 
 class ContentFormModel(forms.ModelForm):
-    # def clean(self):
-    #     cleaned_data = super(ContentFormModel, self).clean()
-    #     content = cleaned_data.get('content')
-    #     if not utility.is_json(content):
-    #         raise forms.ValidationError('The entered content is not a valid JSON string.', code='invalid')
-    #
-    #     return content
-
     class Meta:
         model = ContentModel
-        fields = ['content']
+        fields = ['ref_id']
+
+
+class AboutMeFormModel(forms.ModelForm):
+    class Meta:
+        model = AboutMeModel
+        fields = ['cv', 'bio', 'url', 'name', 'contact', 'github', 'twitter', 'linkedin', 'education_json',
+                  'experience_json', 'publication_json', 'research_area_json', 'skills_t1_json', 'skills_t2_json']
+
+
+class PortfolioFormModel(forms.ModelForm):
+    class Meta:
+        model = PortfolioModel
+        fields = ['projects_json', 'tutorials_json']
