c3b71d7f82a9c3a720c6ec472d96c067555f1b14
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-03-01 17:44:15 +1300

    new admin contents added

--- editor/admin.py
+++ editor/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
-from .models import *
+from .models import ContentModel, AboutMeModel, PortfolioModel
 
 
-class EditorAdmin(admin.ModelAdmin):
-    list_display = ['ref_id', 'content', 'created', 'updated', 'website_name']
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ['ref_id', 'created', 'updated', 'website_name']
 
     class Meta:
         model = ContentModel
 
-admin.site.register(ContentModel, EditorAdmin)
+
+class AboutMeAdmin(admin.ModelAdmin):
+    list_display = ['cv']
+
+    class Meta:
+        model = AboutMeModel
+
+
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ['projects_json', 'tutorials_json']
+
+    class Meta:
+        model = AboutMeModel
+
+admin.site.register(ContentModel, ContentAdmin)
+admin.site.register(AboutMeModel, AboutMeAdmin)
+admin.site.register(PortfolioModel, PortfolioAdmin)
