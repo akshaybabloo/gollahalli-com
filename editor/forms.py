90f315b057169cecf1b8b6cc0146d6310d816f0a
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-03-03 15:47:22 +1300

    name -> first_name, last_name

--- editor/forms.py
+++ editor/forms.py
@@ -7,6 +7,6 @@ class ContentFormModel(forms.ModelForm):
     bio = forms.CharField(widget=forms.Textarea)
     class Meta:
         model = ContentModel
-        fields = ['cv', 'bio', 'url', 'name', "email_id", 'github', 'twitter', 'linkedin', 'education_json',
+        fields = ['cv', 'bio', 'url', 'first_name', 'last_name', 'email_id', 'github', 'twitter', 'linkedin', 'education_json',
                   'experience_json', 'publication_json', 'research_area_json', 'skills_t1_json', 'skills_t2_json',
                   'projects_json', 'tutorials_json']
