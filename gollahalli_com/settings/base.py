78fe268fe3feb122ec6b89548002430aed8893ed
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-05-28 21:38:16 +1200

    n/a

--- gollahalli_com/settings/base.py
+++ gollahalli_com/settings/base.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
+print(os.environ)
 if os.environ['DEBUG'] == 1:
     DEBUG = True
 else:
