802fcfe6cc192bbfab182b888d85d15eb6907c2c
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-05-28 21:46:11 +1200

    int to str

--- gollahalli_com/settings/base.py
+++ gollahalli_com/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ['DEBUG'] == 1:
+if os.environ['DEBUG'] == '1':
     DEBUG = True
     print("In debug mode")
 else:
