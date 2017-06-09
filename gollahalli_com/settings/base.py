237d67a9fcff01c22178f1409874a514527b7b95
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-06-09 18:04:36 +1200

    unwanted imports removed and debug environment added

--- gollahalli_com/settings/base.py
+++ gollahalli_com/settings/base.py
@@ -22,7 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.environ['DEBUG'] == '1':
+    DEBUG = True
+    print("In debug mode")
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
