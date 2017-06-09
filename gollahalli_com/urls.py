3197810236d1fc9f28ba937f3efad52d46a1f841
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-06-09 18:04:05 +1200

    new sitemap structure added

--- gollahalli_com/urls.py
+++ gollahalli_com/urls.py
@@ -1,6 +1,11 @@
 """
 Main URL settings page. See https://docs.djangoproject.com/en/1.10/topics/http/urls/ for more information.
 """
+import datetime
+import json
+import os
+
+import requests
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.sitemaps import views
@@ -9,9 +14,68 @@ from django.http import HttpResponse
 
 from .sitemaps import *
 
+GITHUB_KEY = os.environ['GITHUB_KEY']
+
+
+def get_version():
+    """
+    Get's the latest released version.
+    Returns
+    -------
+    response: dict:
+        A dictionary of GitHub content.
+    """
+    response = requests.get('https://api.github.com/repos/akshaybabloo/gollahalli-me/releases/latest',
+                            auth=('akshaybabloo', GITHUB_KEY))
+    return json.loads(response.text)
+
+
+def github_date_time_format(value):
+    """
+    Strips the date and time of GitHub's format.
+    Parameters
+    ----------
+    value: str
+        The vale should be of format `%Y-%m-%dT%H:%M:%Sz`.
+    Returns
+    -------
+    date: datetime
+        datetime object.
+    """
+    date = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%Sz')
+    return date
+
+
+def custom_date(value):
+    """
+    Strips users date.
+    >>> c_date = custom_date('10/10/2010')
+    Parameters
+    ----------
+    value: str
+        The vale should be of format `%d/%m/%Y`.
+    Returns
+    -------
+    date: datetime
+        datetime object
+    """
+    date = datetime.datetime.strptime(value, '%d/%m/%Y')
+    return date
+
 sitemaps = {
-    'pages': Sitemap(['index'], 1.0),
-    'other': Sitemap(['repo', 'change-log'], 0.5)
+    'pages': Sitemap(
+                ['index'],
+                [1.0],
+                ['monthly'],
+                [datetime.date.today()]
+            ),
+
+    'other': Sitemap(
+                ['repo', 'change-log'],
+                [0.5, 1.0],
+                ['monthly', 'monthly'],
+                [custom_date('10/01/2017'), github_date_time_format(get_version()['published_at'])]
+            )
 }
 
 urlpatterns = [
@@ -27,7 +91,7 @@ urlpatterns = [
         "Sitemap: https://www.gollahalli.com/sitemap.xml\nUser-agent: *\nDisallow: /admin/\nDisallow: /cdn-cgi/",
         content_type="text/plain"), name="robots_file"),
     url(r'^sitemap\.xml$', views.index, {'sitemaps': sitemaps, 'template_name': 'sitemap-index.xml'}),
-    url(r'^sitemap-(?P<section>.+).xml$', views.sitemap, {'sitemaps': sitemaps},
+    url(r'^sitemap-(?P<section>.+).xml$', views.sitemap, {'sitemaps': sitemaps, 'template_name': 'sitemap.xml'},
         name='django.contrib.sitemaps.views.sitemap'),
 ]
 
