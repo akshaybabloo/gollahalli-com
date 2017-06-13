e589b9a6c40d954147c46d6c79dbc4f18b9153be
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-06-13 15:43:29 +1200

    django html-min removed

--- gollahalli_com/settings/base.py
+++ gollahalli_com/settings/base.py
@@ -61,8 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'htmlmin.middleware.HtmlMinifyMiddleware',
-    'htmlmin.middleware.MarkRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'gollahalli_com.urls'
