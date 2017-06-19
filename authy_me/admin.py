9cf121557ceb96de5091025a3259bb9a176d5992
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-06-19 17:10:52 +1200

    authy_me models created and registered

--- authy_me/admin.py
+++ authy_me/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from .models import AuthenticatorModel
+
+
+class AuthenticatorForm(forms.ModelForm):
+    """
+    AuthenticatorModel form for admin view.
+    """
+
+    class Meta:
+        model = AuthenticatorModel
+        fields = '__all__'
+
+
+class AuthenticatorAdmin(admin.ModelAdmin):
+    """
+    AuthenticatorModel admin.
+    """
+    list_display = ['user_id']
+    form = AuthenticatorForm
+
+admin.site.register(AuthenticatorModel, AuthenticatorAdmin)
--- authy_me/models.py
+++ authy_me/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import (User, Group)
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class AuthenticatorModel(models.Model):
+    user_id = models.ForeignKey(User, related_name='auth_user', null=True)
+    first_name = models.CharField(default='First Name', max_length=50)
+    last_name = models.CharField(default='Last Name', max_length=50)
+    phone_number = PhoneNumberField()
+    email_id = models.CharField(default='email ID', help_text='Enter the email ID used to register the Django user', max_length=50)
