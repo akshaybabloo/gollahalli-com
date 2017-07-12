76d18ef78f2fc2748835739fddbe21c48479bf00
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-07-13 11:53:01 +1200

    2fa/register -> security/2fa/register

--- authy_me/urls.py
+++ authy_me/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'login/$', views.log_me_in, name='login'),
     url(r'login/2fa/', views.auth_2fa, name='2fa'),
-    url(r'2fa/register', views.auth_2fa_register, name='2fa_register'),
+    url(r'security/2fa/register', views.auth_2fa_register, name='2fa_register'),
     url(r'^static/js/users\.js', views.users_js, name='users_js'),
 ]
