0c10f74558d4546d87803de3f238b03181cefc46
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-07-19 16:21:23 +1200

    user.phone_number added

--- gollahalli_com/templates/js/users.js
+++ gollahalli_com/templates/js/users.js
@@ -8,12 +8,14 @@ $(document).ready(function () {
                 $("#id_first_name").val('{{user.first_name}}');
                 $("#id_last_name").val('{{user.last_name}}');
                 $("#id_email_id").val('{{user.email}}');
+                $("#id_phone_number").val('{{user.phone_number}}');
             }
         {% endfor %}
         else {
             $("#id_first_name").val('First Name');
             $("#id_last_name").val('Last Name');
             $("#id_email_id").val('email ID');
+            $("#id_phone_number").val('{{user.phone_number}}');
         }
     })
 });
