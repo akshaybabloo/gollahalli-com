2c3d1f4c60961fda6272e9ed47c61b608348a026
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-07-19 21:52:15 +1200

    test_authy_form_fail2 removed

--- authy_me/tests/test_forms.py
+++ authy_me/tests/test_forms.py
@@ -71,17 +71,6 @@ class AuthyFormTests(TestCase):
 
         self.assertTrue(form.is_valid())
 
-    def test_authy_form_fail2(self):
-        """
-        Testing ``AuthyForm`` fail.
-        """
-
-        form_data = {'authy': "123eew#$$", 'is_personal': True}
-        form = AuthyForm(data=form_data)
-        self.assertFalse(form.is_valid())
-        self.assertIn('The string should be all numbers.', str(form.errors))
-        self.assertRaises(forms.ValidationError)
-
 
 class LoginFormTests(TestCase):
     """
