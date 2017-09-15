34e59bf447e27f5e4059e7b298127575844c6cdd
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-09-15 14:49:44 +1200

    user agent added

--- gollahalli_com/sitemaps.py
+++ gollahalli_com/sitemaps.py
@@ -126,5 +126,16 @@ def xsl_content_type():
 
     """
 
-    filename = static('sitemap.xsl')
-    return HttpResponse(filename, content_type="text/xsl")
+    if 'DYNO' in os.environ:
+
+        url = os.path.join(settings.STATIC_URL, 'sitemap.xsl')
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+        headers = {'User-Agent': user_agent, }
+
+        request = urllib.request.Request(url, None, headers)
+        response = urllib.request.urlopen(request)
+        data = response.read().decode('UTF-8')
+    else:
+        data = open(os.path.join(settings.STATIC_ROOT, 'sitemap.xsl')).read()
+
+    return HttpResponse(data, content_type="text/xsl")
