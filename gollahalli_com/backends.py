620497b6dc2b0dbd34d3186578b5588eaa1978e4
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-10-24 19:05:42 +1300

    custom auth backend added

---
+++ gollahalli_com/backends.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+
+
+class HashedPasswordAuthBackend(ModelBackend):
+    """
+    Authenticate based on the hash password.
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            return User.objects.get(username=username, password=password)
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
