19cffcde34ed6442e9e361da8d8ac929976422e6
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-10-25 15:05:59 +1300

    ModelDataExistNode added

--- editor/templatetags/model_tags.py
+++ editor/templatetags/model_tags.py
@@ -1,13 +1,19 @@
 from django import template
-from ..models import ContentModel
+from django.template import NodeList
+
+from editor.models import ContentModel
 
 register = template.Library()
 
 
-@register.filter()
-def check_model_exists():
+@register.tag(name='contentmodelexist')
+def check_model_exists(parser, token):
     """
     Checks if any data available in ContentModel.
+
+    ... {% contentmodelexist ContentModel %}
+    ...     .....
+    ... {% endcontentmodelexist %}
 
     Returns
     -------
@@ -15,8 +21,39 @@ def check_model_exists():
         True or False.
     """
 
-    try:
-        ContentModel.objects.get(ref_id=1)
-        return True
-    except ContentModel.DoesNotExist:
-        return False
+    nodelist = parser.parse(('endcontentmodelexist',))
+    full_node = [(token.split_contents()[1], nodelist)]
+    parser.delete_first_token()
+
+    return ModelDataExistNode(full_node)
+
+
+class ModelDataExistNode(template.Node):
+    """
+    Check if the ``ContentModel`` has data, if ``True`` the context is rendered else empty string is returned.
+    """
+
+    def __init__(self, nodelist):
+        self.list_nodelist = nodelist
+
+    def __iter__(self):
+        for _, nodelist in self.nodelist:
+            yield from nodelist
+
+    @property
+    def nodelist(self):
+        return NodeList(node for _, nodelist in self.list_nodelist for node in nodelist)
+
+    def render(self, context):
+        output = ''
+        for content, nodelist in self.list_nodelist:
+            if content is not None and content == 'ContentModel':
+                try:
+                    ContentModel.objects.get(ref_id=1)
+                    output = self.nodelist.render(context)
+                except ContentModel.DoesNotExist:
+                    output = ''
+            else:
+                output = ''
+
+        return output
