a7757d05a81561fd246821d435776a24226736d7
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-10-25 15:39:06 +1300

    education_content init

--- editor/views.py
+++ editor/views.py
@@ -124,6 +124,60 @@ def content_home(request):
     return render(request, template, context)
 
 
+@login_required()
+def education_content(request):
+    template = "portal/editor/editor_education_index.html"
+
+    try:
+        content = ContentModel.objects.get(ref_id='1')
+    except ContentModel.DoesNotExist:
+        content = False
+
+    form_msg = ''
+
+    if request.method == 'POST':
+        form = ContentModelForm(request.POST)
+
+        if form.is_valid():
+            website_name = form.cleaned_data.get('website_name')
+            cv = form.cleaned_data.get("cv")
+            bio = form.cleaned_data.get("bio")
+            url = form.cleaned_data.get("url")
+            first_name = form.cleaned_data.get("first_name")
+            last_name = form.cleaned_data.get("last_name")
+            email_id = form.cleaned_data.get("email_id")
+            github = form.cleaned_data.get("github")
+            twitter = form.cleaned_data.get("twitter")
+            linkedin = form.cleaned_data.get("linkedin")
+            file = form.cleaned_data.get("file")
+            image = form.cleaned_data.get("image")
+
+            content_model, created = ContentModel.objects.update_or_create(ref_id=1,
+                                                                           website_name=website_name,
+                                                                           cv=cv,
+                                                                           bio=bio,
+                                                                           url=url,
+                                                                           first_name=first_name,
+                                                                           last_name=last_name,
+                                                                           email_id=email_id,
+                                                                           github=github,
+                                                                           twitter=twitter,
+                                                                           linkedin=linkedin,
+                                                                           file=file,
+                                                                           image=image)
+
+            if created:
+                content_model.save()
+                form_msg = "Updates saved"
+                content = content_model
+    else:
+        form = ContentModelForm()
+
+    context = {'form': form, 'content': content, 'form_msg': form_msg}
+
+    return render(request, template, context)
+
+
 @login_required
 def meta_home(request):
     """
