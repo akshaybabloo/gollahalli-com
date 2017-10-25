150c2a778970b3574546ece9f07ede9a13678ba8
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-10-25 15:28:47 +1300

    urls and nav bar updated

--- editor/urls.py
+++ editor/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
     url(r'portal/$', views.portal_home, name='portal_home'),
     url(r'portal/editor$', views.editor_home, name='editor_home'),
     url(r'portal/editor/content$', views.content_home, name='content_home'),
+    url(r'portal/editor/education', views.content_home, name='education_home'),
+    url(r'portal/editor/project', views.content_home, name='project_home'),
+    url(r'portal/editor/tutorial', views.content_home, name='tutorial_home'),
+    url(r'portal/editor/experience', views.content_home, name='experience_home'),
+    url(r'portal/editor/skills', views.content_home, name='skills_home'),
+    url(r'portal/editor/publication', views.content_home, name='publication_home'),
     url(r'portal/editor/meta', views.meta_home, name='meta_home'),
 ]
