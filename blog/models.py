d53f1378256b137a09435ae340fe789c5e70a59f
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-11-10 13:42:26 +1300

    blog model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class BlogModel(models.Model):
+    """
+    Blog model.
+    """
+
+    ref_id = models.IntegerField(auto_created=True, default=1, primary_key=True, serialize=False)
+    title = models.CharField(max_length=1000)
+    body = models.TextField()
+    tags = models.CharField(max_length=10000)
+    author = models.ForeignKey(User, related_name='blog_model', on_delete=None)
+    meta_title = models.CharField(max_length=70)
+    meta_body = models.CharField(max_length=156)
+    code_injection_header = models.CharField(max_length=10000)
+    code_injection_footer = models.CharField(max_length=10000)
+    twitter_card_title = models.CharField(max_length=1000)
+    twitter_card_body = models.CharField(max_length=10000)
+    twitter_card_image = models.ImageField(null=True, blank=True)
+    facebook_card_title = models.CharField(max_length=1000)
+    facebook_card_body = models.CharField(max_length=10000)
+    facebook_card_image = models.ImageField(null=True, blank=True)
+    created = models.DateTimeField(auto_now=True)
+
