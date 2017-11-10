1c1230efe52796ce6eba2004303824086253ca37
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-11-10 16:42:36 +1300

    on post create new super user added

--- welcome/views.py
+++ welcome/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+from welcome.forms import WelcomeForm
+from welcome.utils import check_users
 
 
 def home(request, register='new_registration'):
@@ -21,7 +25,27 @@ def home(request, register='new_registration'):
 
     template = 'welcome/welcome.html'
 
-    context = {'check_db_connection': False, 'check_ssl': False, 'check_users': False, 'check_authy': False,
-               'check_smtp': False, 'check_aws': False, 'check_cloudinary': False}
+    if not check_users():
+
+        if request.method == 'POST':
+            form = WelcomeForm(request.POST)
+            if form.is_valid():
+                first_name = form.cleaned_data.get('first_name')
+                last_name = form.cleaned_data.get('last_name')
+                username = form.cleaned_data.get('username')
+                email = form.cleaned_data.get('email')
+                password = form.cleaned_data.get('first_name')
+
+                user_model, created = User.objects.create_superuser(username=username, email=email, password=password,
+                                                                    first_name=first_name, last_name=last_name)
+                if created:
+                    user_model.save()
+        else:
+            form = WelcomeForm()
+
+    else:
+        return redirect('portal_home')
+
+    context = {'form': form}
 
     return render(request, template, context)
