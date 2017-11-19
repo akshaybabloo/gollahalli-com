c78d136807365be04f32b450b454e8962979f559
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-11-20 08:47:01 +1300

    comments added

--- welcome/views.py
+++ welcome/views.py
@@ -65,7 +65,9 @@ def home(request, options=None):
     return render(request, template, context)
 
 
-# JSON objects
+# ============================================================================================
+#                                      JSON Objects
+# ============================================================================================
 
 def check_db_conn(request):
     """
