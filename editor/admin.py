787b77e716b662ad55535d63b3464ee77a895132
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-12-18 12:10:17 +1300

    created removed

--- editor/admin.py
+++ editor/admin.py
@@ -3,9 +3,10 @@ from .models import *
 
 
 class EditorAdmin(admin.ModelAdmin):
-    list_display = ['ref_id', 'content', 'created', 'updated', 'website_name']
+    list_display = ['ref_id', 'content', 'updated', 'website_name']
 
     class Meta:
         model = ContentModel
+
 
 admin.site.register(ContentModel, EditorAdmin)
