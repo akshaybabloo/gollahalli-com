cf85792fdc19b0f047e6d71c34863266ac8040a7
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-12-18 12:10:01 +1300

    on save update date time

--- editor/models.py
+++ editor/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
@@ -5,6 +6,10 @@ from django.contrib.postgres.fields import JSONField
 class ContentModel(models.Model):
     ref_id = models.IntegerField(primary_key=True)
     content = JSONField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
     website_name = models.CharField(max_length=300, default="Enter your companies name")
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.updated = datetime.datetime.now()
+        return super(ContentModel, self).save()
