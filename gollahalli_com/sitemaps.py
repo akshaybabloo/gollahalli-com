ba261c159bffdfd19b308f3a0eaa6bd6bdbae07c
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-12-18 17:00:56 +1300

    Revert "print and unwanted vars removed"
    This reverts commit ad8588d515befb4adfc4d955769796f5810f3b16.

--- gollahalli_com/sitemaps.py
+++ gollahalli_com/sitemaps.py
@@ -135,7 +135,6 @@ def index_view(request, sitemaps,
     maps = sitemaps.values()
 
     all_sites_lastmod = True
-
     sites = []  # all sections' sitemap URLs
     lastmod = []
     for section, site in sitemaps.items():
@@ -162,6 +161,8 @@ def index_view(request, sitemaps,
                 lastmod.append(site_lastmod[0])
 
     content['sitemaps'] = list(zip(sites, lastmod))
+
+    print(content)
 
     response = TemplateResponse(request, template_name, content,
                                 content_type=content_type)
