5789c61105175181f7b504afea07a85c602e390c
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-02 19:01:19 +1300

    PostAdmin updated

--- blog/admin.py
+++ blog/admin.py
@@ -6,10 +6,23 @@ from blog.models import PostModel
 
 class PostAdmin(admin.ModelAdmin):
     """
-    AuthenticatorModel admin.
+    PostAdmin admin.
     """
-    list_display = ['author', 'title']
+    list_display = ['title', 'slug', 'author', 'publish', 'status', 'tag_list']
+    list_filter = ['status', 'created', 'publish', 'author', 'tags']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
     form = PostForm
+
+    def get_queryset(self, request):
+        return super(PostAdmin, self).get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
 
 
 admin.site.register(PostModel, PostAdmin)
