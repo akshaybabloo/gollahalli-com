4222a8f3b12be280c7c7adcac6d0c2d6e857686d
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-03 18:44:16 +1300

    urls updated for posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'blog/$', views.home, name='blog_home'),
+    url(r'blog/$', views.post_list, name='post_list'),
+    url(r'blog/(?P<year>\d{4})/(?P<month>\d{2})/(?P<date>\d{2})/(?P<post>[-\w]+)', views.post_detail,
+        name='post_detail'),
 ]
